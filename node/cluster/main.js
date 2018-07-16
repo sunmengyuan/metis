b86d32b0180a7aa56de43160350b8cef958646d1
sunmengyuan <sunmengyuan@meituan.com>
2018-07-16 16:19:02 +0800

    log cluster 实验

---
+++ node/cluster/main.js
@@ -0,0 +1,41 @@
+const OS = require('os');
+const Cluster = require('cluster');
+const Log4JS = require('log4js');
+const Koa = require('koa');
+const App = new Koa();
+
+Log4JS.configure({
+    appenders: {
+        console: {
+            type: 'console'
+        },
+        file: {
+            type: 'file',
+            filename: './logs/info.log'
+        }
+    },
+    categories: {
+        default: {
+            appenders: ['console', 'file'],
+            level: 'info'
+        }
+    }
+});
+const Logger = Log4JS.getLogger();
+Logger.level = 'info';
+Logger.info('message');
+
+switch (process.env.MODE) {
+    case 'cluster':
+        if (Cluster.isMaster) {
+            for (let i = 0; i < OS.cpus().length; i++) Cluster.fork();
+            console.log('master', process.pid);
+        } else {
+            App.listen(3000);
+            console.log('worker', process.pid);
+        }
+        break;
+    default:
+        App.listen(3000);
+        break;
+}
